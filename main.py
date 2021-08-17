f0afe639590cb3da76d9fe8b5860c092eb07b0a3
chweesee <81509124+chweesee@users.noreply.github.com>
2021-08-17 18:04:06 +0530

    load -> reload

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ async def unload(ctx, extension):
 @commands.has_permissions(ban_members=True)
 async def reload(ctx, extension):
     """Use this to reload individual cogs. Usage: $reload <cog_name>"""
-    bot.unload_extension(f'cogs.{extension}')
-    bot.load_extension(f'cogs.{extension}')
+    bot.reload_extension(f'cogs.{extension}')
     await ctx.send(f'Cog {extension } is now reloaded!')
 
 @bot.command()
@@ -45,8 +44,7 @@ async def reload(ctx, extension):
 async def reloadall(ctx):
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
-            bot.unload_extension(f'cogs.{filename[:-3]}')
-            bot.load_extension(f'cogs.{filename[:-3]}')
+            bot.reload_extension(f'cogs.{filename[:-3]}')
             await ctx.send(f'Cog {filename[:-3]} is now reloaded!')
 
 #@unload.error
