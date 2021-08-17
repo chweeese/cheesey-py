f9ca206b15861620662daa503c3e723b5360fe69
chweesee <81509124+chweesee@users.noreply.github.com>
2021-08-17 16:47:54 +0530

    Trying slash commands
    discord-interactions slash commands

---
+++ cogs/Slash Commands.py
@@ -0,0 +1,25 @@
+from discord_slash import SlashCommand
+from discord_slash import cog_ext
+
+import discord
+
+from discord.ext import commands
+
+class SlashCog(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot=bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Slash Commands cog is now live!')
+   
+    guild_ids = [848576409312165908]
+
+    @cog_ext.cog_slash(name="test", description="Test", guild_ids=guild_ids)
+    async def test(self, ctx: SlashContext):
+        await ctx.send(content="Testing")
+
+
+def setup(bot):
+    bot.add_cog(SlashCog(bot))
