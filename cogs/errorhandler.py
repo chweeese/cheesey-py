80e3418b3450894fdfc4bfa05371577456176e9d
chweesee <81509124+chweesee@users.noreply.github.com>
2021-08-20 17:05:37 +0530

    Bugfix

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -38,7 +38,7 @@ async def export_exception(ctx, error):
         embed=discord.Embed()
         embed.title="An error occured"
         embed.description="The error has been reported to the devs"
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed, delete_after=20)
 class ErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -55,7 +55,6 @@ class ErrorHandler(commands.Cog):
         embed.set_footer(text=self.bot.user.display_name,icon_url=self.bot.user.avatar_url)
         embed.set_author(name=ctx.message.author.display_name,icon_url=ctx.message.author.avatar_url)
         error = getattr(error, 'original', error)
-        return await ctx.send(embed=embed, delete_after=20)
 
 
         if isinstance(error, discord.errors.Forbidden):
@@ -66,7 +65,7 @@ class ErrorHandler(commands.Cog):
                 embed.add_field(name=f'Error code:',value= f'**{error.code} Forbidden**')
             if error.status!=None:
                 embed.add_field(name=f'Error status:',value= f'**{error.status}**')
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
 
         if isinstance(error, commands.errors.MissingPermissions):
@@ -76,7 +75,7 @@ class ErrorHandler(commands.Cog):
                 missperm=str(perms).replace('_',' ').capitalize()
                 permlist=permlist+missperm+'\n'
             embed.description=f'**{permlist}**'
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
 
         if isinstance(error, commands.errors.MissingRequiredArgument):
@@ -84,15 +83,15 @@ class ErrorHandler(commands.Cog):
             params=ctx.prefix+ctx.command.name+" "+ctx.command.signature#await remote(ctx.command.callback)
             embed.description=str(params)
             embed.set_footer(text='<> Denotes required argument. [] Denotes optional argument')
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.CommandNotFound):
             embed.title=f"Specified command doesn't exist"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.MemberNotFound):
             embed.title=f"Could not find the specified member"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.BadArgument):
             embed.title=f"Invalid argument(s) \N{CROSS MARK}"
@@ -100,24 +99,24 @@ class ErrorHandler(commands.Cog):
             for invalid in error.args:
                 invalidargs=invalidargs+invalid+'\n'
             embed.description=f'**{invalidargs}**'
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.NoPrivateMessage):
             embed.title=f"Error \N{CROSS MARK}"
             embed.description=f'`{ctx.prefix}{ctx.command.name}` Does not work in private messages'
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.CheckFailure):
             embed.title=f"One of the checks failed to validate"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.DisabledCommand):
             embed.title=f"This command has been disabled"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.TooManyArguments):
             embed.title=f"More than required arguments were passed to this command"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.CommandOnCooldown):
             embed.title=f"You are currently on a cooldown"
@@ -129,30 +128,30 @@ class ErrorHandler(commands.Cog):
                  embed.description=f"**You must wait `{int(m)}` minutes and `{int(s)}` seconds to use this command!**"
             else:
                  embed.description=f"**You must wait `{int(h)}` hours, `{int(m)}` minutes and `{int(s)}` seconds to use this command!**"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.NotOwner):
             embed.title=f"Only the owner can use this command"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.MessageNotFound):
             embed.title=f"Couldn't find the requested message"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.UserNotFound):
             embed.title=f"Couldn't find the requested user"
             embed.description=error.argument
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.MissingAnyRole):
             embed.title=f"You do not have the requested roles for this command"
             embed.description=error.argument
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.MissingRole):
             embed.title=f"You do not have the requested role for this command"
             embed.description=error.argument
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.BotMissingPermissions):
             embed.title=f"I do not have the permissions to execute the action"
@@ -162,20 +161,20 @@ class ErrorHandler(commands.Cog):
                 missperm=str(perms).replace('_',' ').capitalize()
                 permlist=permlist+missperm+'\n'
             embed.add_field(name='Missing perms:',value=f'**{permlist}**')
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
 
         if isinstance(error, commands.errors.NSFWChannelRequired):
             embed.title=f"Command can only be executed in NSFW channels"
             embed.description=error.argument
-            return await ctx.send(embed=embed)   
+            return await ctx.send(embed=embed, delete_after=20)   
 
         if isinstance(error, commands.errors.ExtensionAlreadyLoaded):
             embed.title=f"Cog is already loaded"
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=embed, delete_after=20)
         
         if isinstance(error, commands.errors.ExtensionNotLoaded):
             embed.title=f"Cog could not be found"
-            return await ctx.send(embed=embed)                            
+            return await ctx.send(embed=embed, delete_after=20)                            
 
         await export_exception(ctx, error)
         #raise error
